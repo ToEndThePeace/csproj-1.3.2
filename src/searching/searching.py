01dc2515ab4d09199ae2f60cd36d39359de867aa
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-07-22 10:18:15 -0700

    Implemented agnostic binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -9,11 +9,24 @@ def binary_search(arr, target, start, end):
     if target == arr[mid]:
         return mid
     elif target < arr[mid]:
-        # change thsi
         return binary_search(arr, target, start=start, end=mid-1)
     else:
         return binary_search(arr, target, start=mid+1, end=end)
 
+
+def desc_binary_search(arr, target, start, end):
+    # This honestly doesn't work I just copy pasted it oops
+    if end < start:
+        return -1
+
+    mid = (start + end) // 2
+
+    if target == arr[mid]:
+        return mid
+    elif target > arr[mid]:
+        return desc_binary_search(arr, target, start=start, end=mid-1)
+    else:
+        return desc_binary_search(arr, target, start=mid+1, end=end)
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -22,5 +35,13 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively
 # or iteratively
+
 def agnostic_binary_search(arr, target):
-    pass
+    if arr[0] > arr[1]:
+        is_ascending = False
+    else:
+        is_ascending = True
+    if is_ascending:
+        return binary_search(arr, target, 0, len(arr) - 1)
+    else:
+        return desc_binary_search(arr, target, 0, len(arr) - 1)
